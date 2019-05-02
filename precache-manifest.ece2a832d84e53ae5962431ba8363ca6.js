4ed395436a5b54ddbf709f97de0b076b586ef20a
Bilgihan Kose <bilgihankose@gmail.com>
2019-05-03 00:33:37 +0300

    Updates

---
+++ precache-manifest.ece2a832d84e53ae5962431ba8363ca6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72507e9e4fc79891932aaa78d81589c3",
+    "url": "/react-httpcals/index.html"
+  },
+  {
+    "revision": "7c475895ccd65d6bed48",
+    "url": "/react-httpcals/static/css/main.cff5c0a8.chunk.css"
+  },
+  {
+    "revision": "453ded4a47a1a1a2e733",
+    "url": "/react-httpcals/static/js/2.774c5952.chunk.js"
+  },
+  {
+    "revision": "7c475895ccd65d6bed48",
+    "url": "/react-httpcals/static/js/main.62d91e9e.chunk.js"
+  },
+  {
+    "revision": "8ddaf480fc439cc68e3a",
+    "url": "/react-httpcals/static/js/runtime~main.46b03444.js"
+  }
+]);
